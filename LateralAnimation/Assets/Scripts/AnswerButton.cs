9d0e2f8fb1dedcc99967f4779dc76c8cfd2c8afe
Shamira <marshallscm@gmail.com>
2019-07-23 12:03:56 -0700

    Working on event handler for answer buttons

---
+++ LateralAnimation/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+
+    public Text answerText;
+
+    private AnswerData answerData;
+    private TriviaController triviaController;
+
+    // Use this for initialization
+    void Start () 
+    {
+        triviaController = FindObjectOfType<TriviaController> ();
+    }
+
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.answerText;
+    }
+
+
+    public void HandleClick()
+    {
+       triviaController.AnswerButtonClicked (answerData.isCorrect);
+        Debug.Log("*************************");
+    
+    }
+}
