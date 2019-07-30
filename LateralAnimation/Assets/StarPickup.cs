f2557332af02918077b4b68d35602af904a54f4b
Shamira <marshallscm@gmail.com>
2019-07-30 11:37:51 -0700

    Added audio for star pickup.

--- LateralAnimation/Assets/StarPickup.cs
+++ LateralAnimation/Assets/StarPickup.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class StarPickup : MonoBehaviour
 {
+
+    [SerializeField] AudioClip coinPickupSFX;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
